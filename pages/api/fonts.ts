51444650787436b47f685f8fdb2578dc04cdcc4e
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-10-31 20:19:40 +0100

    Add API key

--- pages/api/fonts.ts
+++ pages/api/fonts.ts
@@ -3,16 +3,19 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const url = require('url');
 const needle = require('needle');
 
+const { API_KEY } = process.env;
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   try {
     const params = new URLSearchParams({
+      key: API_KEY,
       ...url.parse(req.url, true).query,
     });
 
-    const apiRes = await needle('get', `https://fonts.googleapis.com/css?${params}`);
+    const apiRes = await needle('get', `https://www.googleapis.com/webfonts/v1/webfonts?${params}`);
 
     if (apiRes.statusCode === 400) {
       res.status(500).json({ error: 'failed to load data' });
